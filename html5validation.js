a46a73f21cb1f46ff895864e7ab5e634fd3048ec
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2011-11-13 12:53:33 +0000

    add email type validation

--- html5validation.js
+++ html5validation.js
@@ -11,68 +11,72 @@
 (function( $ ){
 
   var methods = {
+      // initialization method
       init : function( options ) { 
-        
+        // default options
         var defaults = {
           'error_class' : 'invalid',
         };
-        
         var options = $.extend({}, defaults, options); 
-        
+        // form element
         var form = $(this);
+        // bind form onsubmit action
         form.bind('submit', function () {
           form.html5formvalidation('validation', options );
         });
-
         // bind all form elements
         form.find('input, textarea, select').bind('change keyup', function () {
           $(this).html5formvalidation('elementValidation', options);
         });
       },
+      
+      // validatoin on all form elements
       validation : function( options ) {
+        // indicaties if form is valid
         var is_valid = true;
-        
+        // foreach form element do the validation
         this.find('input, textarea, select').each(function () {
           is_valid = ($(this).html5formvalidation('elementValidation', options) ? is_valid : false);          
         });
         
         return is_valid;
       },
+      
+      // element validation
       elementValidation : function( options ) { 
-        
         var element = $(this);
+        // element type
+        var type = element.attr('type');
+        // indicates if element is valid
         var elem_is_valid = true;
-
+        // remove error class (if not null or empty) from this element before validate
         options.error_class ? element.removeClass(options.error_class) : null;
 
         // if has required tag
         if (element.attr('required')) 
         {
-          var type = element.attr('type');
           // for different kind of types do different validation...
           if (type == 'checkbox' || type == 'radio')
           {        
+            // verifies if one of the elements with this name has been checked
             if (!$('[name*="'+element.attr('name')+'"]:checked')) 
             {
-              options.error_class ? $('[name*="'+element.attr('name')+'"]').addClass(options.error_class) : null;
               elem_is_valid =false;
+              // add error class for every elements with the same name
+              options.error_class ? $('[name*="'+element.attr('name')+'"]').addClass(options.error_class) : null;
             }
             else
             {
+              // removes error class for every elements with the same name
               options.error_class ? $('[name*="'+element.attr('name')+'"]').removeClass(options.error_class) : null;
             }
           }
           else 
           {
             // for other input types verifies if value is empty
-            if(element.val().trim() == '')
-            {
-              elem_is_valid = false;
-              options.error_class ? element.addClass(options.error_class) : null;
-            }
+            element.val().trim() == '' ? elem_is_valid = false : null;
           }
         }
-        
         
         // if has pattern tag
         if (element.attr('pattern')) 
@@ -80,12 +84,34 @@
           // get the pattern
           var pattern = element.attr('pattern');
           // if pattern don't match     
-          if(!element.val().match('^'+pattern+'$'))
+          !element.val().match('^'+pattern+'$') ? elem_is_valid = false : null;
+        }
+        
+        // if has a special type...
+        if (type == 'email') 
+        {
+          // verify if has email format
+          // regular expression for email
+          var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+          // if multiple attribute
+          if(element.attr('multiple'))
           {
-            elem_is_valid = false;
-            options.error_class ? element.addClass(options.error_class) : null;
+            // splitting on commas (,)
+            var emails = element.val().split(',');
+            for(var i=0; i<emails.length; i++)
+            {
+              (emails[i] == '' || !emailReg.test(emails[i])) ? elem_is_valid = false : null;
+            }
+          } 
+          else
+          {
+            !emailReg.test(element.val()) ? elem_is_valid = false : null;
           }
         }
+        
+        
+        // if element is not valid, apply the error class if not null
+        !elem_is_valid ? (options.error_class ? element.addClass(options.error_class) : null) : null;
         
         return elem_is_valid;
       },
